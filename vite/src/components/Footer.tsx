15a3b5aca01c44fdb76881f3baff2d98534fc6ce
inevitablechange <yongho7783@gmail.com>
2024-06-25 21:52:13 +0900

    Refactor Footer.tsx

--- vite/src/components/Footer.tsx
+++ vite/src/components/Footer.tsx
@@ -1,28 +1,31 @@
 import { FC } from "react";
 
+const navList = ["Report", "Contact Us", "Terms of Service", "Privacy"];
+const social = ["discord", "instagram", "twitter", "youtube"];
+
 const Footer: FC = () => {
   return (
     <footer className="pt-[70px] min-h-[250px] border-solid border-t-[0.5px] border-primary/[0.5]">
       <div className="container-style px-10 h-full flex flex-col text-center md:flex-row md:justify-between md:text-left">
-        <div className="flex flex-wrap gap-x-24 max-w-[800px] mx-auto md:mx-0 mb-4">
+        <section className="flex flex-wrap gap-x-24 max-w-[800px] mx-auto md:mx-0 mb-4">
           <div className="flex flex-col gap-4">
-            <span className="hover:font-semibold">Report</span>
-            <span className="hover:font-semibold">Contact Us</span>
-            <span className="hover:font-semibold">Terms of Service</span>
-            <span className="hover:font-semibold">Privacy</span>
+            {navList.map((v, i) => (
+              <span key={i} className="hover:font-semibold">
+                {v}
+              </span>
+            ))}
           </div>
-        </div>
-        <div>
+        </section>
+        <section>
           <nav className="flex gap-6 mb-8 justify-center md:justify-end">
-            <img src="/images/social/discord.png" alt="discord" />
-            <img src="/images/social/instagram.png" alt="instagram" />
-            <img src="/images/social/twitter.png" alt="twitter" />
-            <img src="/images/social/youtube.png" alt="youtube" />
+            {social.map((v, i) => (
+              <img key={i} src={`/images/social/${v}.png`} alt={v} />
+            ))}
           </nav>
           <div>
-            <div>Copyright © 2024 PARTY ENT. All rights reserved.</div>
+            <p>Copyright © 2024 PARTY ENT. All rights reserved.</p>
           </div>
-        </div>
+        </section>
       </div>
     </footer>
   );
